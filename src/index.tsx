55ec38bce8436441369af8e0e42569133273cb79
Яков Панов <diegoling33@yandex.ru>
2021-03-05 01:12:16 +0300

    Added index export

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import {FlexBox, FlexBoxProps, FXAlign, FXJustify } from "./flex/FlexBox";
+import {FlexBoxSpacing, FlexBoxSpacingProps } from "./flex/FlexBoxSpacing";
+import { Normalize } from "./Normalize/Normalize";
+
+export type {
+	FlexBoxProps,
+	FlexBoxSpacingProps,
+}
+
+export {
+	FlexBox,
+	FXJustify,
+	FXAlign,
+
+	FlexBoxSpacing,
+
+	Normalize,
+}
